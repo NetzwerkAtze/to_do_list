721af2b64ec7d015dc3f81ff7e6dd329406246d4
NetzwerkAtze <paultristram@web.de>
2025-03-06 23:45:21 +0000

    finished functions and logic.

--- src/main.py
+++ src/main.py
@@ -1,30 +1,63 @@
 from task_manager import *
-
+import json
 
 def main():
 
-    command_list = ["add_task", "display_tasks", "mark_task_as_done", "delete_task", "end_program"]
-    running = True
+    command_list = ["add_task", "display_tasks", "mark_task_as_done", "delete_task"]
+    exit_choice = len(command_list) + 1
 
     print("Welcome to To-Do-List!\n")
 
-    while running == True:
+    while True:
         print_menu()
         try:
-            choice = int(input("What would you like to do?\n"))-1
-            
-            func_info = command_mapping[command_list[choice]]
+            choice = int(input("What would you like to do?\n"))
+
+            if choice == exit_choice:
+                confirmation = ""
+                while confirmation != "y" and confirmation != "n":
+                    confirmation = input("Save Task list? (y/n)")
+                if confirmation == "y":
+                    save_task_list(task_list)
+
+                confirmation = ""
+                while confirmation != "y" and confirmation != "n":
+                    confirmation = input("End program? (y/n)")
+                if confirmation == "y":
+                    break
+                continue
+
+            func_info = command_mapping[command_list[choice-1]]
             func = func_info["function"]
             fixed_params = func_info["fixed_params"]
             user_params = func_info["user_params"]
 
             args = {}
             for param in user_params:
-                args[param] = input(f"Please input {param}: ")
+                print(f"{param["help"]}\n")
+
+                while True:
+                    user_input = input(f"{param["name"]}: ")
+                    if param["type"] == str:
+                        user_input = user_input.capitalize()
+
+                    if param["type"] == int:
+                        try:
+                            user_input = int(user_input)
+                        except ValueError:
+                            print("\nPlease enter a number.\n")
+                            continue
+
+                    if "valid_values" in param and user_input not in param["valid_values"]:
+                        print("\nPlease enter a valid value.\n")
+                        continue
+
+                    break   
+
+                args[param["name"]] = user_input
             
             func(*fixed_params, **args)
-
-            print(task_list)
+            input("Press Enter to continue...")
 
         except (NameError, IndexError, ValueError):
             print("\ninvalid input\n")
--- src/task_manager.py
+++ src/task_manager.py
@@ -1,33 +1,80 @@
 from datetime import datetime
+import json
 
-task_list = []
+try: 
+    with open("/home/netzwerk_atze/python-projects/to_do_list/data/tasks.json", "r") as file:
+        task_list = json.load(file)
+except FileNotFoundError:
+    task_list = []
+
 task_time = datetime.now()
 formatted_task_time = task_time.strftime("%Y-%m-%d %H:%M:%S")
+task_priority_list = ("high", "medium" ,"low")
 
 def print_menu():
-    print("1. add task\n2. display tasks\n3. mark task as done\n4. delete task\n5. end\n")
+    print("\nMain Menu\n\n1. add task\n2. display tasks\n3. mark task as done\n4. delete task\n5. save & end\n")
 
-def add_task(task_list, formatted_task_time, task_name, task_priority):
-    new_task = {"name": task_name, "priority": task_priority, "time": formatted_task_time}
-    task_list.append(new_task)
+def save_task_list(task_list):
+    with open("/home/netzwerk_atze/python-projects/to_do_list/data/tasks.json", "w") as file:
+        json.dump(task_list, file, indent=4)
+    
+def add_task(task_list, formatted_task_time, task_priority_list, task_name, task_priority):
+    for task in task_list:
+        if task["name"] == task_name.capitalize():
+            print("Task is already existing.")
+            break
+    else:
+        new_task = {"name": task_name, "priority": task_priority_list[task_priority-1], "time": formatted_task_time, "done": False}
+        task_list.append(new_task)
+        print("Task added.")
 
-def display_tasks():
-    print()
+def display_tasks(task_list):
+    if not task_list:
+        print("The task list is empty!")
+    else:
+        print("\nTask list:")
+        for task in task_list:
+            task_done = "[ ]"
+            if task["done"] == True:
+                task_done = "[x]"
+            print(f"name: {task["name"]}    priority: {task["priority"]}    time: {task["time"]}    done: {task_done}\n")
 
-def mark_task_as_done():
-    print()
+def mark_task_as_done(task_list, task_name):
+    for task in task_list:
+        if task["name"].capitalize() != task_name.capitalize():
+            print("Task not found.")
+        elif task["done"] == True:
+            print("Task already marked as done.")
+        elif task["name"].capitalize() == task_name.capitalize():
+            task["done"] = True
+            print("\nTask marked as done.\n")
 
-def delete_task():
-    print()
-
-def end_program():
-    running = False
-    return running
+def delete_task(task_list, task_name, task_choice):
+    for task in task_list:
+        if task["name"].capitalize() != task_name.capitalize():
+            print("Task not found.")
+        task_list[:] = [task for task in task_list if task["name"] != task_name]
+        print("\nTask deleted.\n")
 
 command_mapping = {
     "add_task": {
         "function": add_task, 
-        "fixed_params": [task_list, formatted_task_time],
-        "user_params": ["task_name", "task_priority"] 
+        "fixed_params": [task_list, formatted_task_time, task_priority_list],
+        "user_params": [{"name": "task_name", "type": str, "help": "Enter the name of the task"}, {"name": "task_priority", "type": int, "valid_values": (1, 2, 3), "help":"\nchoose a priority:\n\n 1. high\n 2. medium\n 3. low"}] 
+        },
+    "display_tasks": {
+        "function": display_tasks, 
+        "fixed_params": [task_list],
+        "user_params": [] 
+        },
+    "mark_task_as_done": {
+        "function": mark_task_as_done,
+        "fixed_params": [task_list],
+        "user_params": [{"name": "task_name", "type": str, "help": "Enter the name of the task"}]
+        },
+    "delete_task": {
+        "function": delete_task,
+        "fixed_params": [task_list],
+        "user_params": [{"name": "task_name", "type": str, "help": "Enter the name of the task"}] 
         }
     }
